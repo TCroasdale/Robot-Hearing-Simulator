8823835cb831f9906937cd8eb2be2f54e2e93257
Thomas Croasdale <tecroasdale@gmail.com>
2018-11-11 23:22:51 +0000

    added html template

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+import os
+
+template_dir = os.path.abspath('web/templates')
+app = Flask(__name__, template_folder=template_dir)
+
+
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html', title='Home')
 
 if __name__ == "__main__":
     app.run()
